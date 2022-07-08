52ac74b0345a2484faadae13ae3d178e62d35923
Rodrigo Sousa <rodrigo.ross.comp@gmail.com>
2022-07-07 22:15:22 -0400

    refactor: :lipstick: adicionado botao para abrir ticket

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="flex flex-row justify-between content-center">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight align-middle">
+                {{ __('Dashboard') }}
+            </h2>
+
+            <x-button>
+                <a href="{{ route('tickets.create') }}">
+                    {{ __('New Ticket') }}
+                </a>
+            </x-button>
+        </div>
     </x-slot>
 
     <div class="py-12">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth'])->group(function () {
+    Route::resource('tickets', TicketController::class);
+});
+
+require __DIR__ . '/auth.php';
