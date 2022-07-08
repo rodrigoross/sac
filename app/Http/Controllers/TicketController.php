9f1b7a549cae8f0eb3f2031391cc86d651c57001
Rodrigo Sousa <rodrigo.ross.comp@gmail.com>
2022-07-08 09:38:47 -0400

    feat: :sparkles: salva ticket e seus anexos

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class TicketController extends Controller
 {
@@ -35,7 +38,39 @@ class TicketController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->only(
+            ['type', 'subject', 'description']
+        );
+
+        /**
+         * Substring gerada usando codigo de https://stackoverflow.com/a/13212994
+         *
+         * @author A. Cheshirov
+         */
+        $codigo = substr(str_shuffle(str_repeat($x = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(9 / strlen($x)))), 1, 9);
+
+        $ticket = Auth::user()->tickets()->create(
+            [
+                ...$validated,
+                'code' => $codigo
+            ]
+        );
+
+        if ($request->hasFile('attachments')) {
+            $paths = [];
+
+            foreach ($request->file('attachments') as $file) {
+                $path = Storage::putFile("tickets/{$ticket->code}/", $file);
+
+                $paths[] = ['path' => $path];
+            }
+
+            $ticket->attachments()->createMany($paths);
+        }
+
+        return redirect()
+            ->route('tickets.show', $ticket->code)
+            ->with('success', "Ticket criado com sucesso!");
     }
 
     /**
